b7b49d767cd76a3bf7694ca22e21e445b89b2909
goreleaser <noreply@github.com>
2026-08-01 04:45:32 +0000

    Brew formula update for hmon version v0.7.2

--- Formula/hmon.rb
+++ Formula/hmon.rb
@@ -5,21 +5,21 @@
 class Hmon < Formula
   desc "Terminal dashboard for a small fleet of Linux machines over SSH"
   homepage "https://github.com/Sousf/hmon"
-  version "0.7.1"
+  version "0.7.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Sousf/hmon/releases/download/v0.7.1/hmon_0.7.1_darwin_amd64.tar.gz"
-      sha256 "6a44986e9085c8e956d2a7382c1e2426465d44c4f30da3c58881693d4e89720c"
+      url "https://github.com/Sousf/hmon/releases/download/v0.7.2/hmon_0.7.2_darwin_amd64.tar.gz"
+      sha256 "52fae0ac9f5819ef3b32cb0b14dcf5518a22d6a517984db95f20ac717ce9a0cb"
 
       define_method(:install) do
         bin.install "hmon"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Sousf/hmon/releases/download/v0.7.1/hmon_0.7.1_darwin_arm64.tar.gz"
-      sha256 "30f218728a782da55815f2f2f1001bbec228f1b19965d869657b0cdf032bbd8b"
+      url "https://github.com/Sousf/hmon/releases/download/v0.7.2/hmon_0.7.2_darwin_arm64.tar.gz"
+      sha256 "29b5e04f155741ca1ebf2e54a6332085819c4b2a51d1157a6823c68ae27d1f90"
 
       define_method(:install) do
         bin.install "hmon"
@@ -29,15 +29,15 @@ class Hmon < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Sousf/hmon/releases/download/v0.7.1/hmon_0.7.1_linux_amd64.tar.gz"
-      sha256 "750801c58aa5ea56390d3a05086a32519cc6dbe223f1453cb2de5f4a7a376a14"
+      url "https://github.com/Sousf/hmon/releases/download/v0.7.2/hmon_0.7.2_linux_amd64.tar.gz"
+      sha256 "dac6b719c8f0ae5fa040ba5e772bc7153e78e9c0be90a7186667318b7e7bb6fe"
       define_method(:install) do
         bin.install "hmon"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Sousf/hmon/releases/download/v0.7.1/hmon_0.7.1_linux_arm64.tar.gz"
-      sha256 "7bd678bbc189daa0e22116c423e78aa7855af1920b22803bab76f82817149507"
+      url "https://github.com/Sousf/hmon/releases/download/v0.7.2/hmon_0.7.2_linux_arm64.tar.gz"
+      sha256 "1da83782bfc80d6a121798b5200de8715a2c3cba30ee9f8dfe283638c2f22a35"
       define_method(:install) do
         bin.install "hmon"
       end
